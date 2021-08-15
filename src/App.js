e3d141dbf7fd775dd1e046924d7dc42af4b6756d
Amine Azariz <amine@azariz.com>
2021-08-15 21:39:13 +0100

    (Unstable) Adding the capability to select a slot and get informatons (title, start and end time). + Adding the capability of doing REST Ajax call from the component to a dummy API in preparation for connecting with Backend.

--- src/App.js
+++ src/App.js
@@ -2,29 +2,71 @@ import React from 'react'
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
+/* CSS */
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const propTypes = {}
+/* GLOBALS
+ * @TODO: Use a separate config, env config or database for settings */
+const API_URI = "https://api.example.com/"
 
 class App extends React.Component {
   state = {
     events: []
   };
 
+  sendToApi = ({ start, end, title }) => {
+    /* construct content to send to api */
+    const postBody = {
+      event_start: start,
+      event_end: end,
+      event_title: title
+    }
+    console.log(start, end, title)
+    console.log(postBody)
+
+    /* construct request */
+    const requestMetadata = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postBody)
+    };
+
+    /* request API */
+    fetch(API_URI, requestMetadata)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('Response OK from API.')
+          console.log(result)
+          return true
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow exceptions.
+        (error) => {
+          alert("Error from the API. Check console.")
+          console.error(error)
+          return false
+        }
+      )
+  }
+
   handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
+    const title = window.prompt('Objet du meeting ...')
     if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title,
-          },
-        ],
-      })
+      if (this.sendToApi({ start, end, title }) === true)
+        this.setState({
+          events: [
+            ...this.state.events,
+            {
+              start,
+              end,
+              title,
+            },
+          ],
+        })
   }
 
   render() {
@@ -35,9 +77,8 @@ class App extends React.Component {
           selectable
           localizer={localizer}
           events={this.state.events}
-          defaultView={Views.WEEK}
-          scrollToTime={new Date(1970, 1, 1, 6)}
-          defaultDate={new Date(2021, 8, 14)}
+          defaultView={Views.WEEK}  // choose WEEK view
+          defaultDate={new Date()}  // start on today
           onSelectEvent={event => alert(event.title)}
           onSelectSlot={this.handleSelect}
         />
@@ -45,7 +86,5 @@ class App extends React.Component {
     )
   }
 }
-
-App.propTypes = propTypes
 
 export default App;
