cbe6326df5039cd2a3d86668044bc2d1284779a5
Amine Azariz <amine@azariz.com>
2021-08-15 21:27:42 +0100

    Using the selectable configuration of big-calendar and support of drag/drop to select time slots like Google Calendar.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import React from 'react'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = momentLocalizer(moment);
+const propTypes = {}
 
-class App extends Component {
+class App extends React.Component {
   state = {
-    events: [
-      {
-        start: moment().toDate(),
-        end: moment()
-          .add(1, "days")
-          .toDate(),
-        title: "Some title"
-      }
-    ]
+    events: []
   };
 
+  handleSelect = ({ start, end }) => {
+    const title = window.prompt('New Event name')
+    if (title)
+      this.setState({
+        events: [
+          ...this.state.events,
+          {
+            start,
+            end,
+            title,
+          },
+        ],
+      })
+  }
+
   render() {
+    const localizer = momentLocalizer(moment)
     return (
-      <div className="App">
+      <>
         <Calendar
+          selectable
           localizer={localizer}
-          defaultDate={new Date()}
-          defaultView="month"
           events={this.state.events}
-          style={{ height: "100vh" }}
+          defaultView={Views.WEEK}
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2021, 8, 14)}
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={this.handleSelect}
         />
-      </div>
-    );
+      </>
+    )
   }
 }
+
+App.propTypes = propTypes
 
 export default App;
